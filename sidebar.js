2ac3f77e7f885e45b69194c34cc846fdf7403b27
Piergiorgio Gonni <pg@sekei.ca>
2022-08-01 14:00:00 -0400

    First commit

---
+++ sidebar.js
@@ -0,0 +1,4 @@
+const handleClick = (data, villager) => {
+  data.currentVillager = villager;
+  data.isOpen = true;
+};
---
+++ tailwindConfig.js
@@ -0,0 +1,27 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      borderRadius: {
+        '6xl': '3rem',
+      },
+      colors: {
+        neutral: {
+          100: '#FFF7DF',
+          200: '#FAEFD4',
+          800: '#805120',
+          900: '#664624',
+        },
+        primary: '#55C49F',
+      },
+      fontFamily: {
+        rounded: ['ui-rounded', '-apple-system', 'BlinkMacSystemFont'],
+      },
+      height: {
+        22: '5.5rem',
+      },
+      width: {
+        22: '5.5rem',
+      },
+    },
+  },
+};
